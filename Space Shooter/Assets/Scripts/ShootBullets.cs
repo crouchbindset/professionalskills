0950151433c2d1bfdfc9632d6c61fedc5d13ea29
Sehun Babatunde <37783660+SehunBaba@users.noreply.github.com>
2019-04-07 20:07:59 +0100

    The multiplayer test game

---
+++ Space Shooter/Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootBullets : NetworkBehaviour {
+
+	[SerializeField]
+	private GameObject bulletPrefab;
+
+	[SerializeField]
+	private float bulletSpeed;
+
+	void Update () {
+		if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space)) {
+			this.CmdShoot ();
+		}
+	}
+
+	[Command]
+	void CmdShoot() {
+		GameObject bullet = Instantiate (bulletPrefab, this.transform.position, Quaternion.identity);
+		bullet.GetComponent<Rigidbody2D> ().velocity = Vector2.up * bulletSpeed;
+		NetworkServer.Spawn (bullet);
+		Destroy (bullet, 1.0f);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnEnemies : NetworkBehaviour {
+
+	[SerializeField]
+	private GameObject enemyPrefab;
+
+	[SerializeField]
+	private float spawnInterval = 1.0f;
+
+	[SerializeField]
+	private float enemySpeed = 1.0f;
+
+	public override void OnStartServer () {
+		InvokeRepeating ("SpawnEnemy", this.spawnInterval, this.spawnInterval);
+	}
+
+	void SpawnEnemy() {
+		Vector2 spawnPosition = new Vector2 (Random.Range(-4.0f, 4.0f), this.transform.position.y);
+		GameObject enemy = Instantiate (enemyPrefab, spawnPosition, Quaternion.identity) as GameObject;
+		enemy.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.0f, -this.enemySpeed);
+		NetworkServer.Spawn (enemy);
+		Destroy (enemy, 10);
+	}
+
+}
